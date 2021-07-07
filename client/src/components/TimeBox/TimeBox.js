f3ec38d15129d345a0ebf811a7dcff199bb390ce
Ananya Joshi <73184612+AJgthb2002@users.noreply.github.com>
2021-07-07 14:06:56 +0530

    Create TimeBox.js

---
+++ client/src/components/TimeBox/TimeBox.js
@@ -0,0 +1,27 @@
+import  React, { useState , useEffect } from 'react';
+import styled from 'styled-components';
+
+export const TimeBox = () => {
+
+    var [date,setDate] = useState(new Date());
+    
+    useEffect(() => {
+        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
+
+    return(
+        <Timecontainer>
+            <p> {date.toLocaleTimeString()}</p>
+        </Timecontainer>
+    )
+};
+
+const Timecontainer = styled.div`
+  color: white;
+`;
+
+export default TimeBox;
