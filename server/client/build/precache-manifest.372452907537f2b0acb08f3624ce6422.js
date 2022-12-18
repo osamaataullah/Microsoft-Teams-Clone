57b7616feec92d062d9e0fbedd23abbc5aa5ee60
Osama Ataullah <61313346+osamaataullah@users.noreply.github.com>
2022-12-18 19:15:29 +0530

    added  new buid

---
+++ server/client/build/precache-manifest.372452907537f2b0acb08f3624ce6422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b5d242e895c8b42eda2bf75980cef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0edd7472580751777f74",
+    "url": "/static/css/main.ae5e7fc5.chunk.css"
+  },
+  {
+    "revision": "cb8c09d32e72d5646c94",
+    "url": "/static/js/2.c7d2643c.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.c7d2643c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edd7472580751777f74",
+    "url": "/static/js/main.7159a1d6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
